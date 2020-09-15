c2c78ced6c0175c9fd6c946605ae70572a26f43f
shenyao2333 <shenyao2333@163.com>
2020-09-15 21:55:39 +0800

    first commit

---
+++ src/main/java/com/example/oauth2/config/AuthorizationServerConfig.java
@@ -0,0 +1,11 @@
+package com.example.oauth2.config;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+/**
+ * @author sy
+ * @date Created in 2020.9.15 21:52
+ * @description
+ */
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+}
